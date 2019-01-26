2068f8394788ce62d605b81ecf698d79c4100818
Lotte Vekeman <lotte.vekeman@student.howest.be>
2019-01-26 21:30:00 +0100

    design filterresults ok

--- src/view/pages/lineup.php
+++ src/view/pages/lineup.php
@@ -79,31 +79,28 @@
         <h2>Resultaten:</h2>
         <ul>
           <?php foreach($results as $result): ?>
-            <li class="result">
-              <a href="index.php?page=detail&amp;id=<?php echo $result['id'];?>">
-                <div>
-                  <p class="labelMoment">
-                    <?php echo $result["time"];?>
-                  </p>
-                  <h3>
-                    <?php echo $result["artist"];?>
 
-                    <sup><?php echo $result["short"];?></sup>
-                  </h3>
-                  <p>
-                  <?php echo $result["name"];?>
-                  </p>
+            <li class="item">
+              <a href="index.php?page=detail&amp;id=<?php echo $result['id'];?>">
+              <article>
+                <div class="infoGradient">
+                  <p class="dayLabel"><?php echo $result['time'];?></p>
+                  <div class="textLabel">
+                    <h3><?php echo $result['artist'];?><sup> <?php echo $result['short'];?> </sup></h3>
+                    <p class="textLabel__name"><?php echo $result['name'];?></p>
+                  </div>
                 </div>
+
                 <div class="gradient">
                   <img class="top"
-                    src="assets/img/<?php echo $result["name"];?>.jpg" width="300px"
-                    alt="Foto van <?php echo $result["artist"];?>"
-                  />
+                      src="./assets/img/isb/<?php echo $result['day'];?>/<?php echo $result['type'];?>/<?php echo $result["imgpath"];?>.jpg" width="300px"
+                      alt="Foto van <?php echo $result['artist'];?>"/>
                 </div>
+              </article>
               </a>
             </li>
+
           <?php endforeach; ?>
         </ul>
       </section>
     </main>
-
